5c76630e3782a0db1d8c6370c0b0714d0e184238
Kanishka Rajput <kanishka6393rajput@gmail.com>
2022-07-05 21:59:44 +0530

    submit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const connect = require("./configs/db");
 const port = process.env.PORT || 8080;
-
-
-
 const app = express();
 const cors =require("cors");
 const jobController = require("./controllers/jobs.controller");
 app.use(express.json());
 app.use(cors());
 
-app.use("/jobs", jobController);
+app.use("/", jobController);
 
 app.listen(port,async()=>{
 
